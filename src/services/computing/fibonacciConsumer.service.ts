557098dffa58a7d616568b97c179d5211d4e91a4
quynt <quynguyenthe000@gmail.com>
2025-11-07 15:59:59 +0700

    get service environment variable from .env file

--- src/services/computing/fibonacciConsumer.service.ts
+++ src/services/computing/fibonacciConsumer.service.ts
@@ -1,4 +1,4 @@
-import { isDev, isTest } from '@/utils/environment';
+import { isDev, isTest } from '@/utils/environment.utils';
 import { spawn } from 'child_process';
 
 class FibonacciConsumerService {
